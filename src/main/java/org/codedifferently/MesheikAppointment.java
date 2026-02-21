7a6b91a38bfbd50db3ab703082319148709908e1
mesheikbrown <Mesheik@github.com>
2026-02-21 15:09:22 -0500

    added variables

---
+++ src/main/java/org/codedifferently/MesheikAppointment.java
@@ -0,0 +1,16 @@
+package org.codedifferently;
+
+public class MesheikAppointment {
+
+    //Setting our variables up
+    private String date;
+    private String time;
+    private String reason;
+
+    public MesheikAppointment(String date, String time, String reason){
+        this.date = date;
+        this.time = time;
+        this.reason = reason;
+    }
+
+}//ends class
