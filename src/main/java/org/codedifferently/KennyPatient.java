c14d2e783f853167add8643210c629d96c00c028
Kenny Edelin <kedelin261@gmail.com>
2026-02-21 10:11:56 -0500

    Made patient class

--- src/main/java/org/codedifferently/KennyPatient.java
+++ src/main/java/org/codedifferently/KennyPatient.java
@@ -1,10 +1,54 @@
 package org.codedifferently;
 
-public class KennyPatient {
+    public class KennyPatient {
 
-    private int id;
+        private int id;
 
-    private String name;
+        private String name;
 
-    private boolean checkedIn;
+        private boolean checkedIn;
 
+
+        public KennyPatient(int id, String name) {
+            this.id = id;
+
+            this.name = name;
+
+            this.checkedIn = false;
+        }
+
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public boolean isCheckedIn() {
+            return checkedIn;
+        }
+
+
+        public void checkIn() {
+            this.checkedIn = true;
+
+        }
+
+
+        public void setName(String name) {
+            this.name = name;
+
+        }
+
+
+        @Override
+        public String toString() {
+            return "KennyPatient{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", checkedIn=" + checkedIn +
+                    '}';
+        }
+    }
